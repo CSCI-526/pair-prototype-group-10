2d86bbe86866679329afbe95a9eca08bf85463bc
GAURAV DEEPAK ZANPURE <gauravzanpure211@gmail.com>
2025-02-15 17:56:34 -0800

    cleaning up DeathZone.cs

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -1,24 +1,3 @@
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-
-// public class DeathZone : MonoBehaviour
-// {
-//     public GameOverManager gameOverManager;
-
-//     void OnTriggerEnter2D(Collider2D other)
-//     {
-//         if (other.CompareTag("Player"))
-//         {
-//             gameOverManager.ShowGameOverScreen(false); // Show "GAME OVER"
-//         }
-//         else if (other.CompareTag("Enemy"))
-//         {
-//             gameOverManager.IncreaseEnemyDefeated(); // Count fallen enemies
-//             Destroy(other.gameObject);
-//         }
-//     }
-// }
-
 using UnityEngine;
 
 public class DeathZone : MonoBehaviour
@@ -29,7 +8,7 @@ public class DeathZone : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            gameOverManager.ShowGameOverScreen(false); // Show "GAME OVER"
+            gameOverManager.ShowGameOverScreen(false);
         }
         else if (other.CompareTag("Enemy"))
         {
@@ -38,10 +17,10 @@ public class DeathZone : MonoBehaviour
             // Check if the enemy is red before counting
             if (enemySprite != null && enemySprite.color == Color.red)
             {
-                gameOverManager.DecreaseEnemyCount(); // Count only red enemies
+                gameOverManager.DecreaseEnemyCount();
             }
 
-            Destroy(other.gameObject); // Remove the enemy from the game
+            Destroy(other.gameObject);
         }
     }
 }
