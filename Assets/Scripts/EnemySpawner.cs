2ab57062b52fb1babfe10c1c914a9ef751c2c1e5
Gaurav-Zanpure <gauravzanpure211@gmail.com>
2025-02-20 15:03:59 -0800

    Added the in-game tutorial

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,9 +4,11 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public float spawnInterval = 2f;
+    public static int enemyCount = 0;
 
     void Start()
     {
+        enemyCount = 0;
         InvokeRepeating("SpawnEnemy", 1f, spawnInterval);
     }
 
@@ -14,5 +16,6 @@ public class EnemySpawner : MonoBehaviour
     {
         Vector3 spawnPos = new Vector3(6, 1, 0);
         Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+        enemyCount++;
     }
 }
